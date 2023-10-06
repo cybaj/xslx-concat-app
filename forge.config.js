4f1064ec4837e8defdef8eae606aed69f83034ce
cybaj <cybaj@naver.com>
2023-10-06 19:03:18 +0900

    apply packaging configuration

--- forge.config.js
+++ forge.config.js
@@ -5,25 +5,28 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+    },
+    {
+      name: "@electron-forge/maker-deb",
       config: {},
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
+      name: "@electron-forge/maker-rpm",
       config: {},
     },
     {
-      name: '@electron-forge/maker-rpm',
-      config: {},
+      name: "@electron-forge/maker-squirrel",
+      config: {
+        // certificateFile: "./cert.pfx",
+        // certificatePassword: process.env.CERTIFICATE_PASSWORD,
+      },
     },
   ],
   plugins: [
     {
-      name: '@electron-forge/plugin-auto-unpack-natives',
+      name: "@electron-forge/plugin-auto-unpack-natives",
       config: {},
     },
   ],
